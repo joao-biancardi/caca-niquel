3b83f0ab7a4cec6d766f785d2e0f2be45bc02e1a
joao-biancardi <57998394+joao-biancardi@users.noreply.github.com>
2021-04-28 00:09:53 -0300

    refactoring

--- index.js
+++ index.js
@@ -1,44 +1,44 @@
-function multiplicador(){
+function multiplicador() {
     const quantidadeDeSlot = 3;
     var imagens = [];
     var msgTela;
     var resultados = [];
-    
+
     imagens.push(
-        "real.png",
-        "realdois.jpg",
-        "realtres.jpg",
-        "dollar.jpg",
-        "dollardois.jpg",
-        "dollartres.jpeg",
-        "Euro.png",
-        "eurodois.jpg",
-        "eurotres.jpg"
+        "./images/real.png",
+        "./images/realdois.jpg",
+        "./images/realtres.jpg",
+        "./images/dollar.jpg",
+        "./images/dollardois.jpg",
+        "./images/dollartres.jpeg",
+        "./images/Euro.png",
+        "./images/eurodois.jpg",
+        "./images/eurotres.jpg"
     );
-    
-    var divImagens = document.querySelector("#imagens");
-    var divResultado = document.getElementById("resultado");
+
+    var divImagens = document.querySelector(".images");
+    var divResultado = document.getElementById("results");
 
     divResultado.classList = "";
     divResultado.innerHTML = "Rodando...";
 
     var rodando = setInterval(rodar, 100);
-    
-    setTimeout(function() {
+
+    setTimeout(function () {
         clearInterval(rodando);
         verifiqueSeGanhou();
     }, 1000);
-    
+
 
     function rodar() {
         for (var i = 0; i < quantidadeDeSlot; i++) {
             var aleatorio = 0;
             var slotName = '.slot-' + (i + 1);
             var slotAtual = divImagens.querySelector(slotName);
-    
+
             aleatorio = Math.floor(Math.random() * imagens.length);
             slotAtual.src = imagens[aleatorio];
-    
+
             resultados[i] = imagens[aleatorio];
         }
     }
@@ -49,9 +49,9 @@ function multiplicador(){
             divResultado.innerHTML = msgTela;
             divResultado.classList = 'won'
         } else {
-            msgTela = "Infelizmente você perdeu!!!";
+            msgTela = "Infelizmente você perdeu!";
             divResultado.innerHTML = msgTela;
             divResultado.classList = 'lost'
         }
     }
-} 
+}
